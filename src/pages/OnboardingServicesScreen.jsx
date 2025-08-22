86f692d486636e3a2e52514b942ce9df64387900
TZ-Porky <btkextensions@gmail.com>
2025-08-22 14:33:52 +0100

    fix: Add the category card.

--- src/pages/OnboardingServicesScreen.jsx
+++ src/pages/OnboardingServicesScreen.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { ALL_CATEGORIES_DATA } from '../data/categoryData';
+import CategoryCard from '../components/categoryCard';
 
 const OnboardingServicesScreen = () => {
     return (
@@ -10,7 +11,7 @@ const OnboardingServicesScreen = () => {
                     <span>2/3</span>
                 </div>
                 <button
-                    className="min-w-0.5 bg-gray-200 text-gray-800 py-2 px-2 sm:py-4 md:py-5 rounded-4xl text-sm sm:text-base md:text-lg font-semibold hover:shadow-lg transition-all duration-200 active:scale-95 font-dm-sans"
+                    className="min-w-0.5 text-gray-900 py-2 px-2 sm:py-4 md:py-5 rounded-4xl text-sm sm:text-base md:text-lg font-semibold hover:shadow-lg transition-all duration-200 active:scale-95 font-dm-sans"
                 >
                     Save & exit
                 </button>
@@ -22,6 +23,16 @@ const OnboardingServicesScreen = () => {
                     <h1 className="text-2xl font-bold text-black leading-tight px-4 font-dm-sans">
                         What are you looking for ?
                     </h1>
+                </div>
+                <div className="p-4">
+                    <div className="grid grid-cols-2 gap-2">
+                        {ALL_CATEGORIES_DATA.map((category) => (
+                            <CategoryCard
+                                key={category.id}
+                                category={category}
+                            />
+                        ))}
+                    </div>
                 </div>
             </div>
 
