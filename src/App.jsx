210205b463cf33337a18796ed3903b9c8788dfad
Karl <tekamkarl@gmail.com>
2025-08-22 09:47:57 +0100

    Fix: resolve conflits

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css'
 
 function App() {
@@ -13,19 +12,3 @@ function App() {
 }
 
 export default App
-=======
-import './App.css'
-
-function App() {
-
-  return (
-    <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
-  )
-}
-
-export default App
->>>>>>> c3c69de706398a8f36908236668908529b614b5f
